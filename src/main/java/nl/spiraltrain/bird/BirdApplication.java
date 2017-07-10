cba8ee56f24d811f951e239c05d4f1dd644dec1c
Carpago Software | Raymond Loman <raymond@carpago.nl>
2017-07-10 13:25:58 +0200

    init

---
+++ src/main/java/nl/spiraltrain/bird/BirdApplication.java
@@ -0,0 +1,12 @@
+package nl.spiraltrain.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdApplication.class, args);
+	}
+}
